e7fae7303ddfaac733a5770d7ad9d33d65618d38
Torbjørn Taskjelle <Torbjorn.Taskjelle@ift.uib.no>
2015-01-13 12:18:19 +0100

    Added setup.py; modified .gitignore

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+
+from distutils.core import setup
+
+setup(name='pyAccuRT',
+      version='0.2',
+      description='Reading and plotting results from AccuRT',
+      author='Torbjørn Taskjelle',
+      author_email='Torbjorn.Taskjelle@ift.uib.no',
+      py_modules=['pyAccuRead']
+      )
