620c4db6c534d1a5a89229cd0892d05181b5d4e2
Torbjorn Taskjelle <Torbjorn.Taskjelle@ift.uib.no>
2016-01-07 15:43:19 +0100

    New method for calculating heating rate.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='pyAccuRT',
-      version='0.4',
+      version='0.5',
       description='Reading and plotting results from AccuRT',
       author='Torbjørn Taskjelle',
       author_email='Torbjorn.Taskjelle@ift.uib.no',
