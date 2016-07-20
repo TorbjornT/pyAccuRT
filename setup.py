0f06fbf839335685777fe69242c2a96525ea6e97
Torbjorn Taskjelle <Torbjorn.Taskjelle@ift.uib.no>
2016-07-20 15:43:33 +0200

    Modified setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from distutils.core import setup
 
 setup(name='pyaccurt',
-      version='0.6',
+      version='0.7',
       description='Reading and plotting results from AccuRT',
       author='Torbjørn Taskjelle',
-      author_email='Torbjorn.Taskjelle@ift.uib.no',
+      author_email='totaskj@gmail.com',
+      url='https://bitbucket.org/torbjornt/pyaccurt/',
+      license='MIT',
       py_modules=['accuread']
       )
