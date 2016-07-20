7564362e1e5c5d8dadac081918a14fd0a908d421
Torbjorn Taskjelle <Torbjorn.Taskjelle@ift.uib.no>
2016-07-20 13:11:12 +0200

    Added some tests for wavelength, depth, irradiance array shape

--- tests/test_read.py
+++ tests/test_read.py
@@ -1,1 +1,29 @@
+import numpy as np
 import unittest
+from accuread.pyAccuRead import PyAccu
+
+class TestAccuRead(unittest.TestCase):
+
+    def setUp(self):
+        self.PA = PyAccu('demo1',basefolder='testdata',
+            scalar=True,iops=True,direct=True,runvarfile='sza.txt')
+
+    def test_wl(self):
+        wl = self.PA.wavelengths
+        self.assertTrue(np.array_equal(np.array([400,500,600,700]),wl))
+
+    def test_depths(self):
+        z = self.PA.depths
+        self.assertTrue(np.array_equal(np.array([0,99999,100001]),z))
+
+    def test_runvar(self):
+        rv = self.PA.runvar
+        self.assertTrue(np.array_equal(np.array([45,60]),rv))
+
+    def test_irr_size(self):
+        irrshape = self.PA.downdata.shape
+        self.assertEqual((3,4,2),irrshape)
+
+
+if __name__ == '__main__':
+    unittest.main()
