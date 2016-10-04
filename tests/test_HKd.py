17bd626305f38519915d60638d67bf85d3ad275e
Torbjorn Taskjelle <Torbjorn.Taskjelle@ift.uib.no>
2016-10-04 17:20:57 +0200

    Array size test for heating rate and Kd

---
+++ tests/test_HKd.py
@@ -0,0 +1,23 @@
+import numpy as np
+import unittest
+from accuread import ReadART
+
+class TestCalculatedVals(unittest.TestCase):
+
+    def setUp(self):
+        self.PA = ReadART('demo1',basefolder='tests/testdata',
+            scalar=True,iops=True,direct=True,sine=True,radiance=True,
+            runvarfile='sza.txt')
+
+    def test_H_size(self):
+        H = self.PA.calc_heatingrate()
+        self.assertEqual((3,4,2),H.shape)
+
+
+    def test_Kd_size(self):
+        Kd = self.PA.diffuse_attenuation()
+        self.assertEqual((2,4,2),Kd.shape)
+
+    def test_Kdint_size(self):
+        Kd = self.PA.diffuse_attenuation(integrated=True)
+        self.assertEqual((2,2),Kd.shape)
