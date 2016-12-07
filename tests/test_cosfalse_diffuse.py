37e007fdc1a6445437ed3ac73bd4a728a39671d9
Torbjorn Taskjelle <Torbjorn.Taskjelle@ift.uib.no>
2016-12-07 14:56:29 +0100

    Added test

---
+++ tests/test_cosfalse_diffuse.py
@@ -0,0 +1,26 @@
+import numpy as np
+import unittest
+from accuread import ReadART
+
+class TestAccuRead(unittest.TestCase):
+
+    def setUp(self):
+        self.PA = ReadART('demo1',basefolder='tests/testdata',
+            cosine=False,diffuse=True,
+            runvarfile='sza.txt')
+
+    def test_diffuse_size(self):
+        irrshape = self.PA.diffuse_down.shape
+        self.assertEqual((3,4,2),irrshape)
+
+    def test_has_meta(self):
+        self.assertTrue(hasattr(self.PA,'wavelengths'))
+        self.assertTrue(hasattr(self.PA,'depths'))
+
+    def test_wl(self):
+        wl = self.PA.wavelengths
+        self.assertTrue(np.array_equal(np.array([400,500,600,700]),wl))
+
+
+if __name__ == '__main__':
+    unittest.main()
