56ecf0d20c4f5b99784b093a9bdf202ae76dedbd
Torbjorn Taskjelle <Torbjorn.Taskjelle@ift.uib.no>
2016-12-09 14:09:34 +0100

    __init__, import, setup

--- accuread/accuread.py
+++ accuread/accuread.py
@@ -8,7 +8,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.io as sio
 from scipy.ndimage.filters import gaussian_filter1d as gaussf
-from file_reading import *
+from .file_reading import *
 
 class ReadART(object):
     '''Reads the output text files from AccuRT, and includes methods for
