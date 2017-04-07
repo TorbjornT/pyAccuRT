7d3695c3fa12e753c6d882bd1db036d485cee0e0
Torbjorn Taskjelle <Torbjorn.Taskjelle@ift.uib.no>
2017-04-07 13:48:58 +0200

    Added two new tests: - for varying number of streams - for case of no irradiance/radiance being read

---
+++ tests/test_nocosine_nruns.py
@@ -0,0 +1,18 @@
+import numpy as np
+import unittest
+from accuread import ReadART
+
+class TestAccuRead(unittest.TestCase):
+
+    def setUp(self):
+        self.PA = ReadART('nocosine',basefolder='tests/testdata',
+            cosine=False,iops=True)
+
+    def test_wl(self):
+        wl = self.PA.wavelengths
+        self.assertTrue(np.array_equal(np.array([400,500,600,700]),wl))
+
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_varstreams.py
@@ -0,0 +1,18 @@
+import numpy as np
+import unittest
+from accuread import ReadART
+
+class TestAccuRead(unittest.TestCase):
+
+    def setUp(self):
+        self.PA = ReadART('varstreams',basefolder='tests/testdata',
+            iops=True)
+
+    def test_wl(self):
+        wl = self.PA.wavelengths
+        self.assertTrue(np.array_equal(np.array([400]),wl))
+
+
+
+if __name__ == '__main__':
+    unittest.main()
